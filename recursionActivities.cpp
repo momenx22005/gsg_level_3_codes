f13fe180c2ddf59b03a46154bc5dfc9f438a8061
momenx22005 <s12324285@stu.najah.edu>
2025-08-12 23:51:51 +0300

    Add files via upload

--- recursionActivities.cpp
+++ recursionActivities.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int count = 0;
+
 void rec(int n, int NotAllowed = 0, string current = "") {
     if (n == 0) {
         cout << current << "\n";
+        count++;
         return;
     }
 
     if (NotAllowed != 1)
-        rec(n - 1, 1, current + "Swimming ");
+        rec(n - 1, 1, current+"Swimming ");
 
     if (NotAllowed != 2)
-        rec(n - 1, 2, current + "Football ");
+        rec(n - 1, 2, current+"Football ");
 
     if (NotAllowed != 3)
-        rec(n - 1, 3, current + "Running ");
+        rec(n - 1, 3, current+"Running ");
 }
 
 int main() {
     int n;
     cin >> n;
     rec(n);
+    cout << "count: " << count << endl;
 }
